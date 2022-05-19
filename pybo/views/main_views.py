2d1e4cb1630fddf4df2431a968a1681180b9f646
jungeol-h <dali2728@gmail.com>
2022-05-19 18:03:20 +0900

    backend settinn

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,8 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
+from pybo.models import Question
 bp = Blueprint('main', __name__, url_prefix='/')
 
 
 @bp.route('/')
-def hello_pybo():
-    return 'Hello, Pybo!'
+def index():
+    question_list = Question.query.order_by(Question.create_date.desc())
+    return render_template('Question_list.html', question_list=question_list)
