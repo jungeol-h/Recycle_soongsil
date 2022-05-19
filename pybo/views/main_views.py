b24363e462d991ff3c1836b741fa08fa9c0ee69f
jungeol-h <dali2728@gmail.com>
2022-05-19 23:57:47 +0900

    merge_test

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,10 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, url_for, render_template
+from werkzeug.utils import redirect
 
-from pybo.models import Question
 bp = Blueprint('main', __name__, url_prefix='/')
+
 
 
 @bp.route('/')
 def index():
-    question_list = Question.query.order_by(Question.create_date.desc())
-    return render_template('Question_list.html', question_list=question_list)
+    return render_template('main.html') 
+    ##return redirect(url_for('question._list'))
