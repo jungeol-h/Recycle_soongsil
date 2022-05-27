f8afd8fa94ad82402f190ea8f493d01c2b39634e
jejejekim <duji1207@gmail.com>
2022-05-28 02:45:25 +0900

    질문등록기능구현

--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -1,6 +1,9 @@
-from flask import Blueprint, render_template
-
+from datetime import datetime
+from flask import Blueprint, render_template, request, url_for
+from werkzeug.utils import redirect
+from ..import db
 from pybo.models import Question
+from ..forms import QuestionForm
 
 bp = Blueprint('question', __name__, url_prefix='/question')
 
@@ -14,3 +17,15 @@ def _list():
 def detail(question_id):
      question = Question.query.get_or_404(question_id)
      return render_template('recycle.html', question=question)
+
+
+@bp.route('/create/', methods=('GET', 'POST'))
+def create():
+    form = QuestionForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        question = Question(subject=form.subject.data, content=form.content.data,
+                            create_date=datetime.now())
+        db.session.add(question)
+        db.session.commit()
+        return redirect(url_for('question._list'))
+    return render_template('Question_form.html', form=form)
