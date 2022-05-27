f60d0043ac4abb1e9e79923b64418740dc20032a
jungeol-h <dali2728@gmail.com>
2022-05-28 07:46:27 +0900

    add main page function - new question (only view)

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, url_for, render_template, request
+from operator import itemgetter
+from flask import Blueprint, url_for, render_template, request, flash
 from werkzeug.utils import redirect
 
-from pybo.models import RecycleItem
+from pybo.models import RecycleItem, Question
 
 
 bp = Blueprint('main', __name__, url_prefix='/')
@@ -11,17 +12,39 @@ bp = Blueprint('main', __name__, url_prefix='/')
 @bp.route('/')
 def index():
     item = RecycleItem.query.order_by(RecycleItem.count.desc())
-    print(item)
+    question_list = Question.query.order_by(Question.create_date.desc())
+    return render_template('main.html',freqitem = item, freqquestion = question_list)
 
-    return render_template('main.html',freqitem = item)
-
-
-@bp.route('/load/', methods=('POST',))
+@bp.route('/load/', methods=["POST"])
 def load():
-    item = request.form['item']
-    return redirect(url_for('result.image_load', item=item))
-    # else:
-    #   
+    itemname = request.form['item']
+    validitem = RecycleItem.query.filter_by(item_name =itemname).first()
+    item = RecycleItem.query.order_by(RecycleItem.count.desc())
+    question_list = Question.query.order_by(Question.create_date.desc())
+    if not validitem:
+        flash("아직 등록되지 않은 쓰레기입니다ㅠㅠ")
+        return render_template('main.html',freqitem = item, freqquestion = question_list)
+    else :
+        return redirect(url_for('result.image_load', item=itemname))
+
+
+@bp.route('/load/<itemname>', methods=["GET"])
+def load2(itemname):
+    name=itemname
+    return redirect(url_for('result.image_load', item=name))
+
+
+@bp.route('/request_img/<item>')
+def request_img(item):
+    recycleitem= RecycleItem.query.filter_by(item_name = item).first()
+    path = 'img/item/'+category+'/' + recycleitem.item_name + '/' + recycleitem.item_name+'.png'
+    return path
+
+@bp.route('/request_text/<item>')
+def request_text(item):
+    recycleitem= RecycleItem.query.filter_by(item_name = item).first()
+    return recycleitem.itemname
+
 
 """
 old version
