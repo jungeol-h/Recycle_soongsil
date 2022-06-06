e3ba91b6c5d5c05eb82f9e1899b1a64b5469bdd4
jungeol-h <dali2728@gmail.com>
2022-06-07 07:01:42 +0900

    finish resultpage

--- pybo/views/result_page.py
+++ pybo/views/result_page.py
@@ -14,9 +14,34 @@ def image_load(item):
     recycleitem.count +=1
     db.session.commit()
     category = recycleitem.item_category
+    si_rec= RecycleItem.query.filter_by(item_category = category)
     sub_path = []
     path = 'img/item/'+category+'/' + recycleitem.item_name + '/' + recycleitem.item_name+'.png'
+    si_rec_count = si_rec.count()
     for i in range(recycleitem.size):
-        sub_path.append('img/item/' + recycleitem.item_name + '/' + recycleitem.item_name+ '-'+str(i) +'.png')
-    return render_template('result_page.html', item = recycleitem, subimage =sub_path, mainimage=path)
+        try:
+            sub_path.append('img/item/' + category +'/'+recycleitem.item_name + '/' + recycleitem.item_name+str(i+1)+'.png')
+        except: 
+            sub_path.append('img/item/' + '숫자' +'/'+str(i+1)+'.png')
+            print('sub_path[i]')
+
+    ## 한줄요약 불러오기
+    try:
+        f = open('pybo/static/img/item/'+ category +'/'+recycleitem.item_name + '/' + recycleitem.item_name + '.txt', 'r', encoding='UTF8')
+        one_sentence = f.readline()
+    except: one_sentence = '한줄 요약이 없습니다.'
+
+
+    ## 처리절차 불러오기
+    try:
+        sequence = []
+        for i in range(recycleitem.size):
+            sequence.append(f.readline())
+    except: sequence.append('내용이 없습니다.')
+
+
+
+
+
+    return render_template('result_page.html', item = recycleitem, subimage =sub_path, mainimage=path, si_rec = si_rec, si_rec_count = si_rec_count, one_sentence = one_sentence, sequence=sequence)
     
